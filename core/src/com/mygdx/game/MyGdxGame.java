0ef183c0fd3739a82d644c4c2f17c4bf22f8e289
jpn506 <jpn506@york.ac.uk>
2016-11-21 12:34:18 +0000

    Added camera and audio to create() in application adaptor

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -5,15 +5,31 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Texture img;
+	private Sound appleEaten;
+	private Music mainMusic;
+
 	@Override
 	public void create () {
+		appleEaten = Gdx.audio.newSound(Gdx.files.internal("snake_get_apple.mp3"));
+		mainMusic = Gdx.audio.newMusic(Gdx.files.internal("main.mp3"));
+
+		mainMusic.setLooping(true);
+		mainMusic.play();
+
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,256,256);
 	}
 
 	@Override
